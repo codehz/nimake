ff96355c38a433b6dcabe139ccb0123c23ec34ee
CodeHz <codehz@outlook.com>
2020-12-16 18:16:00 +0800

    add diamond dep test

--- tests/build.nim
+++ tests/build.nim
@@ -3,12 +3,25 @@ import nimake
 target "a":
   fake = true
   dep "b"
+  dep "c"
   receipt do:
     echo "here"
 
 target "b":
   fake = true
+  dep "d"
   receipt do:
     echo "it's B"
+
+target "c":
+  fake = true
+  dep "d"
+  receipt do:
+    echo "it's C"
+
+target "d":
+  fake = true
+  receipt do:
+    echo "it's D"
 
 handleCLI()
